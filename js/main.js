1c2b7b2d0d2837a514b84d11fdf0232780f93377
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-12-14 07:35:15 +0900

    Change prompt

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 var history = Josh.History({ key: 'JOSH.history' });
-var shell = Josh.Shell({ history: history });
+var shell = Josh.Shell({
+  history: history,
+  prompt: '$'
+});
 
 shell.activate();
