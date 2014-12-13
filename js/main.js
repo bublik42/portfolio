c4b8998ebe85b75134ac02e83e689299638dd2e2
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-12-14 08:26:04 +0900

    Add hello command

--- js/main.js
+++ js/main.js
@@ -4,4 +4,18 @@ var shell = Josh.Shell({
   prompt: '$'
 });
 
+shell.setCommandHandler('hello', {
+  exec: function(cmd, args, callback) {
+    response = [
+      'Hello, this is Genki Sugimoto.',
+      'Thank you for your visit.',
+      'Try <a class="command">help</a> to find out what you can do here.',
+      'Enjoy :)',
+      '',
+      'Tip: you can click commands if you don\'t like typing'
+    ].join('<br>');
+    callback(response);
+  }
+});
+
 shell.activate();
