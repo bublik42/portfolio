e21eee89e20bea1856f0086f8035ecbe49055aa1
Vsevolod <stefkin.v@gmail.com>
2016-03-27 22:55:25 +0300

    Add default gulp task and clean gulpfile up

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
-var streamify = require('gulp-streamify')
-var uglify = require('gulp-uglify')
-var source = require('vinyl-source-stream')
+var streamify = require('gulp-streamify');
+var uglify = require('gulp-uglify');
+var source = require('vinyl-source-stream');
 var sass = require('gulp-sass');
 
 gulp.task('browserify', function() {
-  var bundleStream = browserify('./js/main.js').bundle()
-
-  bundleStream
-    .pipe(source('./js/main.js'))
-    .pipe(streamify(uglify()))
-    .pipe(gulp.dest('./build'))
+    browserify('./js/main.js').bundle()
+        .pipe(source('./js/main.js'))
+        .pipe(streamify(uglify()))
+        .pipe(gulp.dest('./build'));
 });
 
 gulp.task('sass', function() {
-  gulp.src('./css/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./build/css'))
+    gulp.src('./css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./build/css'));
 });
+
+gulp.task('default', ['browserify, sass']);
